3bac706f183cdd9256bc0637dc6051e1702cc929
Unknown <leidy.rondon00@usc.edu.co>
2017-11-20 15:29:12 -0500

    borrar

--- contarpalabras.java
+++ contarpalabras.java
@@ -7,7 +7,11 @@ public class CContarPalabras
   final char eof = (char) - 1;
   char car;
   boolean palabra = false;
+<<<<<<< HEAD
   int ncaracteres = 0, npalabras = 0, nlineas = 0, 
+=======
+  int ncaracteres = 0, npalabras = 0, nlineas = 0, totalA=0;
+>>>>>>> 5f358de... imprime el numero de A en le texto
   
   try
   {
@@ -30,14 +34,23 @@ public class CContarPalabras
     }
     if (car == '\n')//Finaliza una linea
      nlineas++;
+<<<<<<< HEAD
  //if (car == 'a' || car == 'A')//cuenta letras A
  //    totalA++;
+=======
+ if (car == 'a' || car == 'A')//cuenta letras A
+     totalA++;
+>>>>>>> 5f358de... imprime el numero de A en le texto
    }
    System.out.println();
    System.out.println("numeros de caracteres es igual" + ncaracteres );
   System.out.println( "numeros de palabras es igual" + npalabras );
   System.out.println("numeros de lineas es igual" + nlineas);
+<<<<<<< HEAD
  
+=======
+ System.out.prinln ("el numero total de letras A es" + totalA"); 
+>>>>>>> 5f358de... imprime el numero de A en le texto
 }
   catch(IOException ignorada){}
  }   
